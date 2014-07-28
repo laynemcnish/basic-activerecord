1e0dfdcad1c0d6dc62ef3942646fad73473fbf1d
Layne McNish <lmcnish14@gmail.com>
2014-07-28 12:01:20 -0600

    found min amount for each customer

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -3,5 +3,6 @@ require "./lib/customer"
 require "./lib/item"
 require "./lib/orderitem"
 require "./lib/order"
+require "awesome_print"
 
-p Order.maximum("amount").to_f
+ap Order.select("customer_id, amount").group("customer_id").order(customer_id: :asc).minimum("amount")
