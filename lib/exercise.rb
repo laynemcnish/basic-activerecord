538a4a80a13c1905677f048713fd4f462ca9c0ca
Layne McNish <lmcnish14@gmail.com>
2014-07-28 14:29:59 -0600

    destroyed the river kayak

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -5,7 +5,7 @@ require "./lib/orderitem"
 require "./lib/order"
 require "awesome_print"
 
-ap Item.create(name: "kayak01", description: "one person river kayak")
+ap Item.where(name: "kayak01", description: "one person river kayak").destroy_all
 
 
 
